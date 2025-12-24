63a04f505b4fea6b7dd89171b227c00de54540f8
VIPUL070 <sutharvipul007@gmail.com>
2025-12-24 22:25:51 +0530

    Building animated navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import About from "./comoponents/About";
 import Hero from "./comoponents/Hero";
+import Navbar from "./comoponents/Navbar";
 
 function App() {
   return (
       <main className="relative min-h-screen w-screen overflow-x-hidden">
+        <Navbar />
         <Hero />
         <About />
       </main>
