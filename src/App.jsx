8124f7706116171d12a60d937f8d1646ce11b1ec
VIPUL070 <sutharvipul007@gmail.com>
2025-12-24 15:56:14 +0530

    Building About section of the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-
+import About from "./comoponents/About";
 import Hero from "./comoponents/Hero";
 
 function App() {
   return (
       <main className="relative min-h-screen w-screen overflow-x-hidden">
         <Hero />
-
+        <About />
       </main>
   )
 }
