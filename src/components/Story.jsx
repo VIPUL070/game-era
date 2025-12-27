88d6f12f79197957762a531502f208ac7e30fc31
VIPUL070 <sutharvipul007@gmail.com>
2025-12-27 12:50:59 +0530

    Fixing a spelling mistake

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -75,7 +75,7 @@ const Story = () => {
   return (
     <section id="story" className="min-h-dvh w-screen bg-black text-blue-50">
       <div className="flex size-full flex-col items-center py-10 pb-24">
-        <p className="gont-general text-sm uppercase m:text-[10px]">
+        <p className="font-general text-sm uppercase m:text-[10px]">
           the multiversal ip world
         </p>
 
